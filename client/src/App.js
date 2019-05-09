ffbcd7e6a65b48689fc80f04aef4f1a2edf2d162
Poka Tchoneng Hermann <pokatchoneng@gmail.com>
2019-05-09 09:18:21 +0100

    end of the tutorial on medium freecode camp authentification with react and nodejs

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,28 @@
+
+import React, { Component } from 'react';
+import { Link, Route, Switch } from 'react-router-dom';
+import Home from './Home';
+import Secret from './Secret';
+import Login from './Login';
+import withAuth from './withAuth';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/secret">Secret</Link></li>
+        </ul>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/secret" component={withAuth(Secret)} />
+          <Route path="/login" component={Login} />
+        </Switch>
+      </div>
+    );
+  }
+}/*
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
@@ -67,3 +92,4 @@ class App extends Component {
 }
 
 export default App;
+*/
--- server.js
+++ server.js
@@ -2,9 +2,90 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 5000;
+const mongoose = require('mongoose');
+const secret = 'mysecretsshhh';
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+// Import our User schema
+const User = require('./models/User.js');
+const withAuth = require('./middleware');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 
+const mongo_uri = 'mongodb://localhost/react-auth';
+mongoose.connect(mongo_uri, function(err) {
+  if (err) {
+    throw err;
+  } else {
+    console.log(`Successfully connected to ${mongo_uri}`);
+    // POST route to register a user
+    app.post('/api/register', function(req, res) {
+      const { email, password } = req.body;
+      const user = new User({ email, password });
+      user.save(function(err) {
+        if (err) {
+          res.status(500)
+            .send("Error registering new user please try again.");
+        } else {
+          res.status(200).send("Welcome to the club!");
+        }
+      });
+    });
+
+    app.post('/api/authenticate', function(req, res) {
+      const { email, password } = req.body;
+      User.findOne({ email }, function(err, user) {
+        if (err) {
+          console.error(err);
+          res.status(500)
+            .json({
+            error: 'Internal error please try again'
+          });
+        } else if (!user) {
+          res.status(401)
+            .json({
+              error: 'Incorrect email or password'
+            });
+        } else {
+          user.isCorrectPassword(password, function(err, same) {
+            if (err) {
+              res.status(500)
+                .json({
+                  error: 'Internal error please try again'
+              });
+            } else if (!same) {
+              res.status(401)
+                .json({
+                  error: 'Incorrect email or password'
+              });
+            } else {
+              // Issue token
+              const payload = { email };
+              const token = jwt.sign(payload, secret, {
+                expiresIn: '1h'
+              });
+              res.cookie('token', token, { httpOnly: true })
+                .sendStatus(200);
+            }
+          });
+        }
+      });
+    });
+    
+  }
+});
+
+app.get('/api/home', function(req, res) {
+  res.send('Welcome!');
+});
+app.get('/api/secret', withAuth, function(req, res) {
+  res.send('The password is potato');
+});
+app.get('/checkToken', withAuth, function(req, res) {
+  res.sendStatus(200);
+})
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
 });
