3e5be882b05b5ae935460b36a8a45790c2bce572
Poka Tchoneng Hermann <pokatchoneng@gmail.com>
2019-06-06 14:27:06 +0100

    We change the module path for strings in our application

--- server.js
+++ server.js
@@ -562,10 +562,11 @@ app.post('/api/world', (req, res) => {
 
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/public')));
+  
+  app.use(express.static(`${__dirname}/client/public`));
 // Handle React routing, return all requests to React app
   app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/public', 'index.html'));
+    res.sendFile(`${__dirname}/client/public/index.html`);
   });
 }
 
