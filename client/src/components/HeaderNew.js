bc3688fe75df504c1a1e1e3fa95c7b5e65df7ae0
Poka Tchoneng Hermann <pokatchoneng@gmail.com>
2019-07-02 05:35:46 +0100

    We can connect to the system in the new header section

--- client/src/components/HeaderNew.js
+++ client/src/components/HeaderNew.js
@@ -5,35 +5,114 @@ class HeaderNew extends Component{
     render(){
         const {cartTotalNumberPlans, frontEndUser, deconnexion} = this.props.value
         return (
-            <div class="header">
-				<div class="header-new">
-					<div class="logo">
+            <div className="header">
+				<div className="header-new">
+					<div className="logo">
 						<img width="125px" height="125px" src="images/logo/logo.jpg" alt="logo"/>
 					</div>
-					<div class="brand-wrapper">
-						<div class="brand">
+					<div className="brand-wrapper">
+						<div className="brand">
 						Electricité-Informatique-Froid
 						</div>
 					</div>
-					<div class="contact-info">
-						<div class="contact">
-							<div><i class="fa fa-phone"></i>+237 6 96 12 84 27</div>
-							<div><i class="fa fa-envelope"></i>eifconsultingandservices@gmail.com</div>
+					<div className="contact-info">
+						<div className="contact">
+							<div><i className="fa fa-phone"></i>+237 6 96 12 84 27</div>
+							<div><i className="fa fa-envelope"></i>eifconsultingandservices@gmail.com</div>
+							{frontEndUser.connected ? 
+
+								<div>
+										<i className="fa fa-user"></i> {frontEndUser.email}
+										
+								</div>
+								: 
+								<React.Fragment>
+									{/* Nothing to show */}
+								</React.Fragment>
+							}
 						</div>
 					</div>
 				</div>
-				<div class="menu-wrapper">
-					<div class="menu-left">
-						<ul class="menu-list">
+				<div className="menu-wrapper">
+					<div className="menu-left">
+						<ul className="menu-list">
 							<li>Accueil</li>
-							<li>Plans</li>
+							<li>
+								<a id="buttonDropDown">Plans</a>
+                                                
+								<div id="myDropdown" className="dropdown-content">
+									<div className="row">
+										<div className="col-md-5 col-lg-5 col-sm-5 offset-md-1 offset-lg-1 offset-sm-1">
+											<div>
+												<h3>Styles</h3>
+												<ol>
+													<li><a href="#">Tous nos plans de maisons</a></li>
+													<li><a href="#">Maison moderne</a></li>
+													<li><a href="#">Maison contemporaine</a></li>
+													<li><a href="#">Maison traditionelle</a></li>
+												</ol>
+											</div>
+											<div>
+												<h3>Formes</h3>
+												<ol>
+													<li><a href="#">Maison en L</a></li>
+													<li><a href="#">Maison en U</a></li>
+													<li><a href="#">Maison en V</a></li>
+												</ol>
+											</div>
+											<div>
+												<h3>&Eacute;tages</h3>
+												<ol>
+													<li><a href="#">Plain-pied</a></li>
+													<li><a href="#">&Agrave; &eacute;tages</a></li>
+													<li><a href="#">Avec sous-sol</a></li>
+												</ol>
+											</div>
+										</div>
+										<div className="col-md-5 col-lg-5 col-sm-5">
+											<div>
+												<h3>Prix</h3>
+												<ol>
+													<li><a href="#">Plain-pied</a></li>
+													<li><a href="#">&Agrave; &eacute;tages</a></li>
+													<li><a href="#">Avec sous-sol</a></li>
+												</ol>
+											</div>
+											<div>
+												<h3>Compl&eacute;ments</h3>
+												<ol>
+													<li><a href="#">Exemple gratuit de plan &agrave; t&eacute;l&eacute;charger</a></li>
+													<li><a href="#">Les nouveaux plans de maison &agrave; t&eacute;l&eacute;charger</a></li>
+													<li><a href="#">Les coups de coeurs des internautes</a></li>
+													<li><a href="#">Plans en PROMO</a></li>
+												</ol>
+											</div>
+										</div>
+									</div>
+								</div>
+							</li>
 							<li>&Agrave; propos de nous</li>
 							<li>Contact</li>
-							<li>Connexion</li>
+							{
+								frontEndUser.connected ? 
+									<li><Link to='/administrationfrontend'>Administration</Link></li>
+									:
+									<React.Fragment></React.Fragment>
+							}
+							<li>{frontEndUser.connected ? 
+								<span 
+									
+									onClick={()=>{
+										deconnexion()
+									}}>
+										D&eacute;connexion</span>:
+								<Link to="/loginfrontend/home">Connexion</Link> 
+								}
+							</li>
 						</ul>
 					</div>
-					<div class="menu-right">
-						<div class="content">
+					<div className="menu-right">
+						<div className="content">
 							<svg width="28px" height="28px" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
 									 viewBox="0 0 489 489" style={{enableBackground:"new 0 0 489 489"}} xmlSpace="preserve">
 								<g>
